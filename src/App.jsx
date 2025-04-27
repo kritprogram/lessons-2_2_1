74fedadcdb79f765b8f64e02fbd6016b844e8761
Maksim Yuraha <kritprogram@gmail.com>
2025-04-27 23:26:43 +0300

    2_2_3 init

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,26 @@
-import { useState } from 'react';
-import styles from './app.module.css';
-import data from './data.json';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+
+const initial = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '=', 'C', '0', '<x'];
 
 export const App = () => {
-	// Можно задать 2 состояния — steps и activeIndex
-	const [steps, _] = useState(data);
-	const [activeIndex, setActiveIndex] = useState(0);
-
-	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
-
-	function onClickPrev() {
-		setActiveIndex((prev) => prev - 1);
-	}
-	function onClickNext() {
-		setActiveIndex((prev) => prev + 1);
-	}
-	function onClickStart() {
-		setActiveIndex(0);
-	}
-
-	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
-	const isFirstStep = activeIndex === 0;
-	const isLastStep = activeIndex === steps.length - 1;
+	const [result, setResult] = useState('');
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.card}>
-				<h1>Инструкция по готовке пельменей</h1>
-				<div className={styles.steps}>
-					<div className={styles['steps-content']}>{steps[activeIndex].content}</div>
-					<ul className={styles['steps-list']}>
-						{steps.map((step, index) => (
-							<li
-								key={step.id}
-								className={
-									styles['steps-item'] +
-									' ' +
-									(index <= activeIndex ? styles.done : '') +
-									' ' +
-									(index === activeIndex ? styles.active : '')
-								}
-							>
-								<button className={styles['steps-item-button']} onClick={() => setActiveIndex(index)}>
-									{index + 1}
-								</button>
-								{step.title}
-							</li>
-						))}
-					</ul>
-					<div className={styles['buttons-container']}>
-						<button className={styles.button} onClick={onClickPrev} disabled={isFirstStep}>
-							Назад
-						</button>
-						<button className={styles.button} onClick={isLastStep ? onClickStart : onClickNext}>
-							{isLastStep ? 'Начать сначала' : 'Далее'}
-						</button>
+			<div className={styles.output}>{result}</div>
+			<div className={styles.buttonsContainer}>
+				{initial.map((button, index) => (
+					<div
+						key={index}
+						className={`${styles.button}
+						 ${['+', '-'].includes(button) ? styles.operationButton : ''}
+						 ${['C', '<x'].includes(button) ? styles.specialButton : styles.numberButton}
+						  ${button === '=' ? styles.equalButton : ''}`}
+					>
+						{button}
 					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
